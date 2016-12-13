4d7202cdd01cba53b518101412a24af6065f8f27
felipe <fsanchez@undanet.com>
2016-12-13 16:52:30 +0100

    place entity

---
+++ agua/src/AppBundle/Entity/Place.php
@@ -0,0 +1,107 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\PlaceRepository")
+ * @ORM\Table(name="place")
+ */
+class Place {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    /** @ORM\Column(type="string", length=255) */
+    protected $name;
+    /** @ORM\Column(type="text", nullable=true) */
+    protected $description;
+    /** @ORM\Column(type="text", nullable=true) */
+    protected $location;
+    /** @ORM\Colum(type="string", length=255) */
+    protected $picture;
+
+    /**
+     * @return mixed
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * @param mixed $picture
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param mixed $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+}
---
+++ agua/src/AppBundle/Entity/PlaceRepository.php
@@ -0,0 +1,8 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class PlaceRepository extends EntityRepository{
+
+}
